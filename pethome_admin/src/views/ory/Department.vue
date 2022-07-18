1a7f0edfe585ae50c5080e4721369a8187832700
xzzhome <1091526909@qq.com>
2022-07-18 18:02:57 +0800

    部门crud

--- pethome_admin/src/views/ory/Department.vue
+++ pethome_admin/src/views/ory/Department.vue
@@ -2,17 +2,17 @@
 	<section>
 		<!--工具条-->
 		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
-			<el-form :inline="true" :model="filters">
-				<el-form-item>
-					<el-input v-model="filters.name" placeholder="姓名"></el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" v-on:click="getDepartments">查询</el-button>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="handleAdd">新增</el-button>
-				</el-form-item>
-			</el-form>
+      <el-form :inline="true">
+        <el-form-item>
+          <el-input v-model="keyword" placeholder="请输入部门名称或编号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" v-on:click="keywordQuery">关键字查询</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleAdd">新增</el-button>
+        </el-form-item>
+      </el-form>
 		</el-col>
 
 		<!--列表-->
@@ -32,8 +32,8 @@
       </el-table-column>
       <el-table-column prop="name" label="部门名称" width="100" sortable>
       </el-table-column>
-      <el-table-column prop="dirPath" label="部门路径" width="100" sortable>
-      </el-table-column>
+<!--      <el-table-column prop="dirPath" label="部门路径" width="100" sortable>
+      </el-table-column>-->
       <el-table-column prop="manager.username" label="部门经理" width="100" sortable>
       </el-table-column>
       <el-table-column prop="parent.name" label="上级部门" width="100" sortable>
@@ -47,8 +47,8 @@
       </el-table-column>
       <el-table-column label="操作">
         <template scope="scope">
-          <el-button size="small" @click="handleEdit(scope.row)">编辑</el-button>
-          <el-button type="danger" size="small" @click="handleDel(scope.row)">删除</el-button>
+          <el-button size="small" @click="handleEdit(scope.$index,scope.row)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.$index,scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -66,61 +66,55 @@
 			</el-pagination>
 		</el-col>
 
-		<!--编辑界面-->
-		<el-dialog title="编辑" :visible.sync="editFormVisible" :close-on-click-modal="false">
+		<!--编辑界面，模态框-->
+		<el-dialog :title="title" :visible.sync="editFormVisible" :close-on-click-modal="false">
 			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-				<el-form-item label="姓名" prop="name">
+        <el-form-item label="编号" prop="sn">
+          <el-input v-model="editForm.sn" auto-complete="off"></el-input>
+        </el-form-item>
+				<el-form-item label="部门名称" prop="name">
 					<el-input v-model="editForm.name" auto-complete="off"></el-input>
 				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="editForm.sex">
-						<el-radio class="radio" :label="1">男</el-radio>
-						<el-radio class="radio" :label="0">女</el-radio>
+				<el-form-item label="状态">
+					<el-radio-group v-model="editForm.state">
+						<el-radio class="radio" :label="1">启用</el-radio>
+						<el-radio class="radio" :label="0">禁用</el-radio>
 					</el-radio-group>
 				</el-form-item>
-				<el-form-item label="年龄">
-					<el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="生日">
-					<el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
-				</el-form-item>
-				<el-form-item label="地址">
-					<el-input type="textarea" v-model="editForm.addr"></el-input>
-				</el-form-item>
-			</el-form>
+        <!--
+          下拉框的部门经理
+          v-for="item in managers" - 遍历，值放在item
+		      :label="item.username" - 回显哪一个值
+		      :value="item" - 点击保存你要传递到后端的数据
+        -->
+        <el-form-item label="部门经理">
+          <el-select clearable v-model="editForm.manager_id" placeholder="请选择">
+            <el-option v-for="item in managers"
+                       :label="item.username"
+                       :value="item.id">
+              <span style="float: left">{{ item.username }}</span>
+              <span style="float: right; color: #8492a6; font-size: 13px">{{ item.phone }}
+		      </span>
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="上级部门">
+          <el-cascader v-model="editForm.parent_id"
+                       :options="deptTree"
+                       :props="{
+            	checkStrictly: true,
+                label: 'name', // 级联框显示那个属性的值
+                value: 'id'    // 回显时需要,回传值的时候也需要
+            }" clearable>
+          </el-cascader>
+        </el-form-item>
+      </el-form>
 			<div slot="footer" class="dialog-footer">
 				<el-button @click.native="editFormVisible = false">取消</el-button>
 				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
 			</div>
 		</el-dialog>
 
-		<!--新增界面-->
-		<el-dialog title="新增" :visible.sync="addFormVisible" :close-on-click-modal="false">
-			<el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="addForm.name" auto-complete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="addForm.sex">
-						<el-radio class="radio" :label="1">男</el-radio>
-						<el-radio class="radio" :label="0">女</el-radio>
-					</el-radio-group>
-				</el-form-item>
-				<el-form-item label="年龄">
-					<el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="生日">
-					<el-date-picker type="date" placeholder="选择日期" v-model="addForm.birth"></el-date-picker>
-				</el-form-item>
-				<el-form-item label="地址">
-					<el-input type="textarea" v-model="addForm.addr"></el-input>
-				</el-form-item>
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click.native="addFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
-			</div>
-		</el-dialog>
 	</section>
 </template>
 
@@ -136,56 +130,47 @@
         totals: 0,
         currentPage: 1,
         pageSize: 5,
+        //3.高级查询所需数据
+        keyword: null,
 
         filters: {
 					name: ''
 				},
 
-				sels: [],//列表选中列
-
+        managers: [],//经理数据
+        deptTree: [],//部门树
+				sels: [],//列表选中列，左边的勾勾
+        title:'',
 				editFormVisible: false,//编辑界面是否显示
 				editLoading: false,
 				editFormRules: {
+          sn: [
+            { required: true, message: '请输入部门编号', trigger: 'blur' }
+          ],
 					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
+						{ required: true, message: '请输入部门名称', trigger: 'blur' }
 					]
 				},
 				//编辑界面数据
 				editForm: {
-					id: 0,
+					id: null,
+          sn: '',
 					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
+          state: 1,
+          manager_id: null,
+          parent_id: null
 				},
-
-				addFormVisible: false,//新增界面是否显示
-				addLoading: false,
-				addFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				//新增界面数据
-				addForm: {
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				}
-
 			}
 		},
 		methods: {
       //1.获取部门列表
       getDepartments() {
-        //分页参数
+        //分页参数与高级查询参数，进行封装
         let para = {
           currentPage:this.currentPage,
-          pageSize:this.pageSize
-          //name:this.filters.name 高级查询才使用
+          pageSize:this.pageSize,
+          //高级查询参数 - 关键字
+          keyword: this.keyword
         };
         //加载框,忙等框
         this.listLoading = true;
@@ -197,9 +182,14 @@
           this.$message.error("系统繁忙，请稍后重试")
         })
       },
-      //当前页，分页数据展示
+      //2.当前页，分页数据展示
       handleCurrentChange(val) {
         this.currentPage = val;
+        this.getDepartments();
+      },
+      //3.关键字查询
+      keywordQuery(){
+        this.currentPage = 1;
         this.getDepartments();
       },
 			//性别显示转换
@@ -211,111 +201,114 @@
 				this.$confirm('确认删除该记录吗?', '提示', {
 					type: 'warning'
 				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { id: row.id };
-					removeDepartment(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getDepartments();
-					});
-				}).catch(() => {
+          this.listLoading = true;
+          //发送请求
+          this.$http.delete("/department/" + row.id).then(res => {
+            this.listLoading = false;
+            if (res.data.success) {
+              this.$message.success("删除成功")
+            } else {
+              this.$message.error(res.data.msg);
+            }
+            this.getDepartments();//刷新页面
+          }).catch(res => {
+            this.$message.error("网络延迟");
+          })
+        }).catch(() => {
+            //确认框点击取消之后走这里
+          });
 
-				});
 			},
 			//显示编辑界面
 			handleEdit: function (index, row) {
-				this.editFormVisible = true;
+        this.title="编辑"
+        //克隆数据：防止对象引用修改显示的数据
 				this.editForm = Object.assign({}, row);
+        this.getManagers();//点击编辑按钮，执行方法，获得经理数据
+        this.getDeptTree();
+        this.editFormVisible = true;//弹出模态框
 			},
 			//显示新增界面
 			handleAdd: function () {
-				this.addFormVisible = true;
-				this.addForm = {
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
+        this.title="新增"
+				this.editForm = {
+          id: null,
+          sn: '',
+          name: '',
+          state: 1,
+          manager_id: null,
+          parent_id: null,
 				};
+        this.getManagers();//点击新增按钮，执行方法，获得经理数据
+        this.getDeptTree();
+        this.editFormVisible = true;
 			},
-			//编辑
+			//编辑和添加的提交按钮
 			editSubmit: function () {
+        //表单校验
 				this.$refs.editForm.validate((valid) => {
 					if (valid) {
 						this.$confirm('确认提交吗？', '提示', {}).then(() => {
 							this.editLoading = true;
-							//NProgress.start();
+							//克隆数据
 							let para = Object.assign({}, this.editForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							editDepartment(para).then((res) => {
-								this.editLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['editForm'].resetFields();
-								this.editFormVisible = false;
-								this.getDepartments();
-							});
+              this.$http.put("/department",para).then(res=>{
+                this.editLoading = false;//关忙等框
+                this.editFormVisible = false;//关模态框
+                if (res.data.success){
+                  this.$message.success("操作成功")
+                }else {
+                  this.$message.error(res.data.msg)
+                }
+                this.getDepartments();
+              }).catch(res=>{
+                this.$message.error("前端错误")
+              })
 						});
 					}
 				});
 			},
-			//新增
-			addSubmit: function () {
-				this.$refs.addForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.addLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.addForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							addDepartment(para).then((res) => {
-								this.addLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['addForm'].resetFields();
-								this.addFormVisible = false;
-								this.getDepartments();
-							});
-						});
-					}
-				});
-			},
+      //勾选时，将本行数据，读取到前端data里面的sels中
 			selsChange: function (sels) {
 				this.sels = sels;
 			},
 			//批量删除
 			batchRemove: function () {
-				var ids = this.sels.map(item => item.id).toString();
+        // 将数据sels,通过map方法循环导出单个属性id及其值,读取所有勾选的id，组成的数组[1,2,3,4]
+				var ids = this.sels.map(item => item.id);
 				this.$confirm('确认删除选中记录吗？', '提示', {
 					type: 'warning'
-				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { ids: ids };
-					batchRemoveDepartment(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getDepartments();
-					});
+				})
+        .then(() => {
+          this.listLoading = true;
+          //发送请求
+          this.$http.patch("/department/",ids).then(res => {
+            this.listLoading = false;
+            if (res.data.success) {
+              this.$message.success("删除成功")
+            } else {
+              this.$message.error(res.data.msg);
+            }
+            this.getDepartments();//刷新页面
+          }).catch(res => {
+            this.$message.error("网络延迟");
+          })
 				}).catch(() => {
-
+          //确认框点击取消之后走这里
 				});
-			}
+			},
+      //获取部门经理
+      getManagers(){
+        this.$http.get("/employee").then(res => {
+          this.managers = res.data;
+        })
+      },
+      //获取部门树 - 无线级联
+      getDeptTree(){
+        this.$http.get("/department/deptTree").then(res => {
+          this.deptTree = res.data;
+        })
+      }
 		},
 		mounted() {
 			this.getDepartments();
